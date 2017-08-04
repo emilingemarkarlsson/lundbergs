1356ac02dfdc4c5b391141d3aaa1883b38dae76d
Emil Karlsson <emilkarlsson@Emils-Mac-mini.local>
2017-08-04 18:33:16 +0200

    update

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
+const bookController = require('./controllers/book');
 
 /**
  * API keys and Passport configuration.
@@ -112,12 +113,14 @@ app.use((req, res, next) => {
   }
   next();
 });
+
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 /**
  * Primary app routes.
  */
 app.get('/', homeController.index);
+app.get('/books', bookController.getBooks);
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
